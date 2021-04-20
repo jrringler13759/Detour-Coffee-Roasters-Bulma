538e3eeda552ffcfe0de3057cac4b68d0c258361
Nikita <jrringler13759@gmail.com>
2021-04-20 17:05:01 -0400

    added carousel and important link section

---
+++ script.js
@@ -0,0 +1,20 @@
+
+var carousels = bulmaCarousel.attach('.carousel', options);
+
+// Loop on each carousel initialized
+for(var i = 0; i < carousels.length; i++) {
+	// Add listener to  event
+	carousels[i].on('before:show', state => {
+		console.log(state);
+	});
+}
+
+// Access to bulmaCarousel instance of an element
+var element = document.querySelector('#heroCarousel');
+if (element && element.bulmaCarousel) {
+	// bulmaCarousel instance is available as element.bulmaCarousel
+	element.bulmaCarousel.on('before-show', function(state) {
+		console.log(state);
+	});
+}
+
